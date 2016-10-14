610957fead6bf95fb4e6eb054adb4fa7fb6317f5
TimoKinnunen <timo.kinnunen@bredband.net>
2016-10-14 16:46:03 +0200

    This is a view

--- FilmDatabas/Controllers/HomeController.cs
+++ FilmDatabas/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace FilmDatabas.Controllers
 {
@@ -10,6 +6,7 @@ namespace FilmDatabas.Controllers
     {
         public ActionResult Index()
         {
+            // this is view
             return View();
         }
 
